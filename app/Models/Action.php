ab821734d74a23b0ed482bbed7432768d121505c
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-10 22:54:48 +0300

    Fix PHPStan action status typing

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * @property ActionStatus $status
+ */
 class Action extends Model
 {
     protected $guarded = ['id'];
