ac13c8596a1677be3def0d7e846d9578adbcd323
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-14 14:53:33 +0300

    Document evaluation status enum for static analysis

--- app/Models/Evaluation.php
+++ app/Models/Evaluation.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/** @property EvaluationStatus $status */
 class Evaluation extends Model
 {
     protected $guarded = ['id'];
