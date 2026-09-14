dd68d27bedbf61b6aa32e88b2b673911fd3d8a1a
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-14 22:38:04 +0300

    Wire global Livewire resilience hooks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <x-layouts::app.sidebar :title="$title ?? null">
     <x-ui.livewire-feedback />
+    <x-ui.livewire-resilience-script />
     <flux:main class="min-w-0">
         {{ $slot }}
     </flux:main>
