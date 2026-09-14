c7af0946d4b451455ef0ad358e2e854554320865
Proxy for Main <149622634+deputy-proxy@users.noreply.github.com>
2026-09-14 10:55:48 +0300

    #126 add reusable page header

---
+++ resources/views/components/ui/page-header.blade.php
@@ -0,0 +1,14 @@
+<header {{ $attributes->merge(['class' => 'flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between']) }}>
+    <div class="min-w-0">
+        <flux:heading level="1" size="xl" class="truncate">{{ $title }}</flux:heading>
+        @isset($description)
+            <flux:subheading class="mt-1">{{ $description }}</flux:subheading>
+        @endisset
+    </div>
+
+    @isset($actions)
+        <div class="flex shrink-0 flex-wrap items-center gap-2">
+            {{ $actions }}
+        </div>
+    @endisset
+</header>
